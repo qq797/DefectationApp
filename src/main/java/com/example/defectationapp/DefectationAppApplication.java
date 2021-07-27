5b300f52bc040e9d67a7b704d6b852a48ced2147
qq787 <planeta-apple@mail.ru>
2021-07-28 02:02:07 +0300

    Initial commit

---
+++ src/main/java/com/example/defectationapp/DefectationAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.defectationapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefectationAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DefectationAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/defectationapp/DefectationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.defectationapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DefectationAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
